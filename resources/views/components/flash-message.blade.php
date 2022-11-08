1f44a61ba8a50efa109663d5f4eba763f53b3c8a
Nick Vales <nvales@nextvisit.io>
2022-11-08 15:47:05 -0500

    implemented alpinejs to automatically hide toast messages after 3 seconds. closes #6

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,8 +1,9 @@
 @if (session()->has('message'))
-    <div 
+    <div
+        x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"
         class="
             alert alert-danger position-absolute top-0 start-50 translate-middle-x
-            shadow p-3 mt-2 rounded" 
+            shadow p-3 mt-2 rounded"
         role="alert"
     >
         <p>{{ session('message') }}</p>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
             crossorigin="anonymous"
             referrerpolicy="no-referrer"
         />
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
         <title>LaraDrugs | Find Laravel Drugs</title>
